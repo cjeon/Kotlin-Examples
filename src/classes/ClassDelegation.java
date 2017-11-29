4664be4c0a80f0489dd7dc09440be94edd5858d2
cjeon <adobecs5.kaist@gmail.com>
2017-11-29 12:21:14 +0900

    save as you breath 🎶

---
+++ src/classes/ClassDelegation.java
@@ -0,0 +1,8 @@
+package classes;
+
+/**
+ * @Author cjeon
+ * @Since 29/11/2017
+ */
+public class ClassDelegation {
+}
--- src/delegation/Lazy.kt
+++ src/delegation/Lazy.kt
@@ -4,3 +4,33 @@ package delegation
  * @Author cjeon
  * @Since 28/11/2017
  */
+
+/* Lazy : 처음 호출될 때만 Setter가 실행된다. */
+fun main(args : Array<String>) {
+    before()
+    after()
+}
+
+fun before() {
+    println("before")
+
+    var num = 1
+    val two = 2
+    val lazy : Int by lazy { num + two }
+
+    num++
+    println(lazy)
+}
+
+fun after() {
+    println("after")
+    var num = 1
+    val two = 2
+    val lazy : Int by lazy { num + two }
+
+    println(lazy)
+    num++
+    println(lazy)
+}
+
+/* See function definition for more info */
